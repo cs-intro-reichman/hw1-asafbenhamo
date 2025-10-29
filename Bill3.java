c741067679d3b9764956832aea41faf9bbd2c1cd
asafbenhamo <asaf.benhamo@post.runi.ac.il>
2025-10-29 16:05:05 +0200

    complete bill3

--- Bill3.java
+++ Bill3.java
@@ -1,10 +1,21 @@
 // Splits a restaurant bill evenly among three diners.
+
+//import javax.print.DocFlavor.STRING;
+
 public class Bill3 {
 	public static void main(String[] args) {
 		// To get you started, here is the first line in the program:
 	    String name1 = args[0];
-		int i = 5;
-		System.out.println(i);
+		String name2 = args[1];
+		String nanm3 = args[2];
+		int bill = Integer.parseInt(args[3]);
+		double pay = bill / 3.0;
+	      pay = Math.ceil(pay);
+		
+
+         System.out.printf("Dear " + nanm3 + ", " + name2 + ", " + "and " + name1 + ": pay " + pay + " shekels each." );
+		
+	
 	    // Replace this comment with the rest of your code   
 	}
 }
