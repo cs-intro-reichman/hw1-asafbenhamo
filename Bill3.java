0fc681bd184b422655dbd6a77d6553d3342cf61e
asafbenhamo <asaf.benhamo@post.runi.ac.il>
2025-10-29 13:08:53 +0200

    done

--- Bill3.java
+++ Bill3.java
@@ -3,6 +3,8 @@ public class Bill3 {
 	public static void main(String[] args) {
 		// To get you started, here is the first line in the program:
 	    String name1 = args[0];
+		int i = 5;
+		System.out.println(i);
 	    // Replace this comment with the rest of your code   
 	}
 }
