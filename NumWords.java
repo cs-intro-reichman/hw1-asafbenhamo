57bc965706bc9896c9e3d06ebf86409253f3fafd
asafbenhamo <asaf.benhamo@post.runi.ac.il>
2025-10-29 19:34:51 +0200

    num f

--- NumWords.java
+++ NumWords.java
@@ -2,5 +2,12 @@
 public class NumWords {
 	public static void main(String args[]) {
 	    // Replace this comment with your code
+		 int number = Integer.parseInt(args[0]);
+		 int h = number / 100;
+		 int a = number - (h * 100);
+		 a = a / 10 ;
+		 int b = number - (h *100) - ( a * 10) ;
+
+		 System.out.println(h + " hundreds, " + a + " tens, and " + b + " ones.");
 	}
 }
